00bc0ee6b48974d3f3ed276486400f270f427712
farukyildirim <muhammedfaruk2000@hotmail.com>
2024-06-30 15:35:06 +0300

    first commit

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $role
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role = null)
+    {
+        if (!Auth::check() || Auth::user()->role !== $role) {
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>User Page</title>
+</head>
+
+<body>
+    <h1>Welcome, User!</h1>
+</body>
+
+</html>
